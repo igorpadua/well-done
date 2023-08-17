16642d83910db8e469d595a5d32f81ae8bc67720
igorpadua <igorpadua@tutanota.com>
2023-08-17 11:24:37 -0300

    feat: newTask gets e sets

--- newtask.cpp
+++ newtask.cpp
@@ -12,3 +12,53 @@ newTask::~newTask()
 {
     delete ui;
 }
+
+QString newTask::name() const
+{
+    return m_name;
+}
+
+void newTask::setName(const QString &newName)
+{
+    m_name = newName;
+}
+
+QString newTask::description() const
+{
+    return m_description;
+}
+
+void newTask::setDescription(const QString &newDescription)
+{
+    m_description = newDescription;
+}
+
+QDate newTask::startDate() const
+{
+    return m_startDate;
+}
+
+void newTask::setStartDate(const QDate &newStartDate)
+{
+    m_startDate = newStartDate;
+}
+
+QDate newTask::finishDate() const
+{
+    return m_finishDate;
+}
+
+void newTask::setFinishDate(const QDate &newFinishDate)
+{
+    m_finishDate = newFinishDate;
+}
+
+QString newTask::getStatus() const
+{
+    return status;
+}
+
+void newTask::setStatus(const QString &newStatus)
+{
+    status = newStatus;
+}
--- newtask.hpp
+++ newtask.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <QWidget>
+#include <QDate>
 
 namespace Ui {
 class newTask;
@@ -14,7 +15,23 @@ public:
     explicit newTask(QWidget *parent = nullptr);
     ~newTask();
 
+    QString name() const;
+    void setName(const QString &newName);
+    QString description() const;
+    void setDescription(const QString &newDescription);
+    QDate startDate() const;
+    void setStartDate(const QDate &newStartDate);
+    QDate finishDate() const;
+    void setFinishDate(const QDate &newFinishDate);
+    QString getStatus() const;
+    void setStatus(const QString &newStatus);
+
 private:
     Ui::newTask *ui;
+    QString m_name;
+    QString m_description;
+    QDate m_startDate;
+    QDate m_finishDate;
+    QString status;
 };
 
